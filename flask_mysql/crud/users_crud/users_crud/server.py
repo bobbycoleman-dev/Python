58badf3d719e8dee2e5b722bf195183b413ce747
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-08-09 10:06:51 -0500

    update to users crud

--- flask_mysql/crud/users_crud/users_crud/server.py
+++ flask_mysql/crud/users_crud/users_crud/server.py
@@ -8,44 +8,49 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # call the get all classmethod to get all friends
+    """call the get_all classmethod to get all friends"""
     users = User.get_all()
     return render_template("index.html", users=users)
 
 
 @app.route("/create")
 def create():
+    """Redirect to the Create User template"""
     return render_template("create.html")
 
 
 @app.route("/create_user", methods=["POST"])
 def create_user():
+    """Create a new User"""
     User.save(request.form)
     return redirect("/")
 
 
 @app.route("/show/<int:user_id>")
 def show(user_id):
+    """Redirect to the Show User template"""
     user = User.get_one(user_id)
     return render_template("show.html", user=user)
 
 
 @app.route("/update/<int:user_id>")
 def update(user_id):
+    """Redirect to the Update User template"""
     user = User.get_one(user_id)
     return render_template("update.html", user=user)
 
 
 @app.route("/update_user", methods=["POST"])
 def update_user():
-    id = request.form["id"]
-    show = "show/" + id
+    """Update User information and return back to the Show User template"""
+
     User.update(request.form)
-    return redirect(show)
+    return redirect(f"show/{request.form['id']}")
 
 
 @app.route("/delete_user/<int:id>")
 def delete_user(id):
+    """Delete a User from the db"""
     User.delete(id)
     return redirect("/")
 
